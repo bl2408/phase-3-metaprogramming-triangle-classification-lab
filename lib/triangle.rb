52b5a6cf72f0b38ce19d01e4ab7a2ce9fd0e0329
Brian Lambert <brianlambert2408@gmail.com>
2022-11-09 10:06:08 +1100

    commit tree 552d340e45c621571d33f7fba003d014652f186c

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
 class Triangle
   # write code here
+  attr_reader :a, :b, :c
+
+  def initialize a, b, c
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+
+    validate
+
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b==c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+
+  end
+
+  class TriangleError < StandardError
+  end
+
+  private
+
+  def validate
+    [a,b,c].each do |i| 
+      if i < 0
+        raise TriangleError
+      end
+    end
+    raise TriangleError unless a + b > c && a + c > b && b + c > a
+  end
 end
+
+
